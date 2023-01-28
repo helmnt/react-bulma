a74224a77d0fffdc0732b6955f99238b5cb81f01
helmnt <57876799+helmnt@users.noreply.github.com>
2023-01-28 11:04:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bulma
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3ehiku)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Div starts here <div className="">new div</div>
+      </p>
+    </div>
+  );
+}
